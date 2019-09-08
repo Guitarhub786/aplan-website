125d45acc8f31ce70da072823c659ca297a36ff4
Guitarhub786 <codewithdrawal@gmail.com>
2019-09-08 13:47:38 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+  const $gallery = $('.gallery a').simpleLightbox();
+});
+
+function logo() {
+  const logo = document.querySelectorAll('#logo path');
+
+  for (let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+  }
+}
